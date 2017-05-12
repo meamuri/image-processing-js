23952c6f74d5199bae521a65beba18ba2043e080
meamuri <meammf@yandex.ru>
2017-05-12 23:20:11 +0300

    now work brightness and contrast algorithms

---
+++ README.md
@@ -0,0 +1,6 @@
+In this application you can use some image processing algorithms 
+
+It is very simple: you should choose image from file system 
+please, use files from media folder, bocouse this files have good ratio for page layout.
+
+Only move slider and watch effects!
--- src/app.js
+++ src/app.js
@@ -1,62 +1,115 @@
-function initCtx(canvas, img) {
-    canvas.attr('width', img.width);
-    canvas.attr('height', img.height);
-    let ctx = canvas[0].getContext('2d');
-    ctx.clearRect(0, 0, img.width, img.height);
-    ctx.drawImage(img, 0,0, img.width, img.height);
+function initCtx(img, canvas_in, canvas_out) {
+    let ctx_in = canvas_in[0].getContext('2d');
+    let ctx_out = canvas_out[0].getContext('2d');
+
+    img.onload = () => {
+        canvas_in.attr('width', img.width);
+        canvas_in.attr('height', img.height);
+        canvas_out.attr('width', img.width);
+        canvas_out.attr('height', img.height);
+
+        ctx_in.clearRect(0, 0, img.width, img.height);
+        ctx_in.drawImage(img, 0,0, img.width, img.height);
+        ctx_out.clearRect(0, 0, img.width, img.height);
+        ctx_out.drawImage(img, 0,0, img.width, img.height);
+        img.style.display = 'none';
+    };
+
 }
 
 function renderImage(file, canvas_in, canvas_out) {
     let reader = new FileReader();
     // inject an image with the src url
     reader.onload = (event) => {
+        let the_url = event.target.result;
+
         let img = new Image();
-        img.src = event.target.result;
-        initCtx(canvas_in, img);
-        initCtx(canvas_out, img);
+        img.src = the_url;
+
+        initCtx(img, canvas_in, canvas_out);
     };
     // when the file is read it triggers the onload event above.
     reader.readAsDataURL(file);
 }
 
 
-function initAll() {
-    function workWithImage(brightness) {
-        let toByte = (val) => {
-            if (val > 255) {
-                return 255;
-            }
-            if (val < 0)
-                return 0;
-            return val;
-        };
-
-        let ctx = canvas_in[0].getContext('2d');
-        let imageData = ctx.getImageData(0, 0, canvas_in[0].width, canvas_in[0].height);
-        let data = imageData.data;
-
-        let correctBrightness = () => {
-            for (let i = 0; i < data.length; i += 4) {
-                data[i]     = toByte(brightness + data[i]);
-                data[i + 1] = toByte(brightness + data[i + 1]);
-                data[i + 2] = toByte(brightness + data[i + 2]);
-            }
-        };
-        correctBrightness();
-        ctx = canvas_out[0].getContext('2d');
-        ctx.putImageData(imageData, 0, 0);
+const toByte = (val) => {
+    if (val > 255) {
+        return 255;
     }
+    if (val < 0)
+        return 0;
+    return val;
+};
 
+
+function workWithSaturation(saturation, canvas_in, canvas_out) {
+
+}
+
+function workWithContrast(contrast, imageData) {
+    let data = imageData.data;
+
+    let factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
+    for (let i = 0; i < data.length; i += 4) {
+        data[i]     = toByte(factor * (data[i] - 128) + 128);
+        data[i + 1] = toByte(factor * (data[i + 1] - 128) + 128);
+        data[i + 2] = toByte(factor * (data[i + 2] - 128) + 128);
+    }
+}
+
+function workWithBrightness(brightness, imageData) {
+    let data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        data[i]     = toByte(brightness + data[i]);
+        data[i + 1] = toByte(brightness + data[i + 1]);
+        data[i + 2] = toByte(brightness + data[i + 2]);
+    }
+}
+
+function workWithImage(p, canvas_in, canvas_out, work) {
+    let ctx = canvas_in[0].getContext('2d');
+    let imageData = ctx.getImageData(0, 0, canvas_in[0].width, canvas_in[0].height);
+    let ctx_out = canvas_out[0].getContext('2d');
+
+    work(p, imageData);
+    ctx_out.putImageData(imageData, 0, 0);
+}
+
+
+function initAll() {
     let canvas_in = $('#in-canvas');
     let canvas_out = $('#out-canvas');
-    let slider = $("#slider");
-    let brightness = slider.attr('value');
 
-    slider.slider();
-    slider.on("slide", function(slideEvt) {
+    let slider_brightness = $("#slider");
+    let slider_saturation = $("#saturation");
+    let slider_contrast = $("#contrast");
+
+    slider_brightness.slider();
+    slider_saturation.slider();
+    slider_contrast.slider();
+
+    let brightness = slider_brightness.attr('value');
+    let saturation = slider_saturation.attr('value');
+    let contrast = slider_contrast.attr('value');
+
+    slider_brightness.on("slide", function(slideEvt) {
         $(this).attr('value', slideEvt.value);
         brightness = slideEvt.value;
-        workWithImage(brightness);
+        workWithImage(brightness, canvas_in, canvas_out, workWithBrightness);
+    });
+
+    slider_contrast.on("slide", (slideEvt) => {
+        $(this).attr('value', slideEvt.value);
+        contrast = slideEvt.value;
+        workWithImage(contrast, canvas_in, canvas_out, workWithContrast);
+    });
+
+    slider_saturation.on("slide", (slideEvt) => {
+        $(this).attr('value', slideEvt.value);
+        saturation = slideEvt.value;
+        workWithImage(saturation, canvas_in, canvas_out, workWithSaturation);
     });
 
     // handle input changes
@@ -64,5 +117,5 @@ function initAll() {
         // grab the first image in the FileList object and pass it to the function
         renderImage(this.files[0], canvas_in, canvas_out);
     });
-    
+
 }
